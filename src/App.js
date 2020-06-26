756fad506a47ab9bfbab462b4d0a499dbcd55689
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-26 15:13:01 -0300

    Usando el state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Buscador from './components/Buscador'
 
 
-function App() {
-  return (
-    <div className="app container">
-      <div className="jumbotron">
-        <p className="lead text-center">Buscador de imagenes</p>
+class App extends Component {
 
-        <Buscador />
+  state = {
+    termino: 'Cafe'
+  }
+
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    })
+  }
+
+  render() {
+    return (
+      <div className="app container">
+
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+  
+          <Buscador 
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        
+        {this.state.termino}
+      
       </div>
-    </div>
-  );
+    );
+
+  }
 }
 
 export default App;
