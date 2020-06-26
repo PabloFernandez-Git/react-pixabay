75b747f2f3405961c00f14c488177ea7e35f8dea
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-26 12:28:43 -0300

    Boton del formulario

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -9,6 +9,9 @@ class Buscador extends Component {
                     <div className="form-group col-md-8">
                         <input type="text" className="form-control form-control-lg" placeholder="Busca tu imagen" />
                     </div>
+                    <div className="form-group col-md-4">
+                        <input type="submit" className="btn btn-lg btn-danger btn-block" value="Buscar" />
+                    </div>
                 </div>
             </form>
         );
